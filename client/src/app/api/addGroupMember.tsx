fb0de10b2d578899a9372da7f673629f26da971e
Neha Thakur <tneha6260@gmail.com>
2026-06-16 16:37:51 +0545

    adde addmember, leave, delete and remove group api

---
+++ client/src/app/api/addGroupMember.tsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const addGroupMember = async (groupID, memberID) => {
+    try {
+        const { data } = await axios.put(
+          `${process.env.NEXT_PUBLIC_API_URL}/group/addMember`,
+          { groupID, memberID },
+          { withCredentials: true }
+        );
+        if (data) {
+          // return "Added SuccessFully"
+          return data?.message
+        }
+      } catch (error) {
+        if (error.status) {
+          console.log(error.response.data.message);
+          // return "Some Error Occurred";
+          return error.response.data.message
+        }
+      }
+}
+
+export default addGroupMember
---
+++ client/src/app/api/removeGroupMember.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const removeGroupMember = async({groupID, memberID}) => {
+  try {
+
+    const {data} = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/group/removeMember`,{
+        groupID, memberID
+    }, {withCredentials: true});
+    if (data) {
+      return "Removed SuccessFully"
+    }
+  } catch (error) {
+    if (error.status) {
+      console.log(error.response.data.message);
+      return "Some Error Occurred";
+    }
+  }
+}
+
+export default removeGroupMember
